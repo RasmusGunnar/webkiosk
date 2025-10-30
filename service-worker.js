b00b77bde07f74c8c54e15ff937689f7cd1df879
RasmusGunnar <rgjconsult@gmail.com>
2025-10-30 15:33:24 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// service-worker.js (optional; not used by SAFE html unless you register it explicitly)
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => { self.clients.claim(); });
-self.addEventListener('fetch', () => {});
